506a64052b131f3206945054d62a4399ed423acf
Aseer KT <aseermiqdad@gmail.com>
2020-10-03 16:34:55 +0530

    React Apollo Setup

---
+++ client/README.md
@@ -0,0 +1,6 @@
+## Setup
+
+- Delete Unecessary Files
+- `yarn add @apollo/client graphql`
+- `yarn add -D graphql-codegen/cli`
+- `npx graphql-codegen init`
---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+
+function App() {
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+  if (loading) return <div>Loading...</div>;
+  return <div className='App'>{JSON.stringify(data)}</div>;
+}
+
+export default App;
